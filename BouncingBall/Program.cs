e6fab380e987bb16ef4dace46f70487bdd053b7a
KevinLi <kevin.li@sharpgaming.com>
2020-12-23 20:31:11 +0000

    Simple~ once again its about understanding the problem

---
+++ BouncingBall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BouncingBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BouncingBall(3, 0.66, 1.5)); 
+            Console.WriteLine((BouncingBall(30.0, 0.66, 1.5)));
+        }
+        
+        public static int BouncingBall(double h, double bounce, double window) 
+        {
+            if (bounce >= 1 || bounce <= 0 || (window >= h))
+                return -1;
+
+            var occurrence = 1;
+            
+            while (h > window)
+            {
+                h *= bounce;
+                if (h > window)
+                {
+                    occurrence+=2;
+                }
+            }
+            
+            return occurrence;
+        }
+    }
+}
