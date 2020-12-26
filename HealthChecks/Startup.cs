48f46e17578627db814930bf62bb7be4e84279f3
KevinLi <kevin.li@sharpgaming.com>
2020-12-26 16:27:34 +0000

    Each healthcheck is to return a HealthStatus It is recommended to add specific health check extensions to make configuring the services clear in start up Have a WriteResponse helper method to return formatted health status

--- HealthChecks/Startup.cs
+++ HealthChecks/Startup.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using HealthChecks.HealthChecks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
@@ -31,6 +38,13 @@ namespace HealthChecks
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "HealthChecks", Version = "v1"});
             });
+            services.AddHealthChecks()
+                .AddMemoryHealthCheck("memory")
+                .AddCheck<ExampleHealthCheck>(
+                    "example_health_check",
+                    failureStatus: HealthStatus.Degraded,
+                    tags: new[] { "example" })
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +57,15 @@ namespace HealthChecks
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HealthChecks v1"));
             }
 
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                },
+                ResponseWriter = WriteResponse
+            });
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -50,6 +73,48 @@ namespace HealthChecks
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+        
+        private static Task WriteResponse(HttpContext context, HealthReport result)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+
+            var options = new JsonWriterOptions
+            {
+                Indented = true
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new Utf8JsonWriter(stream, options))
+                {
+                    writer.WriteStartObject();
+                    writer.WriteString("status", result.Status.ToString());
+                    writer.WriteStartObject("results");
+                    foreach (var entry in result.Entries)
+                    {
+                        writer.WriteStartObject(entry.Key);
+                        writer.WriteString("status", entry.Value.Status.ToString());
+                        writer.WriteString("description", entry.Value.Description);
+                        writer.WriteStartObject("data");
+                        foreach (var item in entry.Value.Data)
+                        {
+                            writer.WritePropertyName(item.Key);
+                            JsonSerializer.Serialize(
+                                writer, item.Value, item.Value?.GetType() ?? 
+                                                    typeof(object));
+                        }
+                        writer.WriteEndObject();
+                        writer.WriteEndObject();
+                    }
+                    writer.WriteEndObject();
+                    writer.WriteEndObject();
+                }
+
+                var json = Encoding.UTF8.GetString(stream.ToArray());
+
+                return context.Response.WriteAsync(json);
+            }
         }
     }
 }
