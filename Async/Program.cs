fddb7e17a42c8ef3b6342eba0f5e4fc2eb9cd3ba
KevinLi <kevin.li@sharpgaming.com>
2021-01-31 00:38:25 +0000

    async

---
+++ Async/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+
+            // Demonstrate by doing Task.Run, we are putting the work onto a background thread
+            // Which allow the main thread to continue execute
+            Task.Run(() => worker.Work1());
+            Task.Run(() => worker.Work2());
+            Task.Run(() => worker.Work3());
+
+            // This Task<string> is propagated from an external async source (httpClient),
+            // we could get the result string by await this task
+            var result = worker.ThisCouldBeTheServiceLayer();
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Async/Worker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class Worker : IAsyncStateMachine
+    {
+        public void Work1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Work1 Complete!");
+        }
+        
+        public int Work2()
+        {
+            Console.WriteLine("Work2 Complete!");
+            return 10;
+        }
+        
+        public void Work3()
+        {
+            Console.WriteLine("Work3 Complete!");
+        }
+
+        public async Task<string> ThisCouldBeTheServiceLayer()
+        {
+            var theString = await InputOutputN();
+            return theString;
+        }
+
+        private async Task<string> InputOutputN()
+        {
+            var client = new HttpClient();
+            var content = await client.GetStringAsync("whatever");
+
+            return content;
+        }
+
+        public void MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
