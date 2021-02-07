131c4bc86d0f891e156bb40886170d1260bbe563
KevinLi <kevin.li@sharpgaming.com>
2021-02-07 21:58:30 +0000

    gRPC unary & bi-directional calls

---
+++ GrpcService.Server/Services/ChatService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Server.Services
+{
+    public class ChatService : Chat.ChatBase
+    {
+        private readonly ILogger<ChatService> _logger;
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
+        
+        public override async Task SendMessage(IAsyncStreamReader<ClientToServerMessage> requestStream, 
+            IServerStreamWriter<ServerToClientMessage> responseStream, 
+            ServerCallContext context)
+        {
+            var clientToServer = HandleClientToServerPing(requestStream);
+
+            var serverToClient = HandleServerToClient(responseStream, context);
+
+            await Task.WhenAll(clientToServer, serverToClient);
+        }
+
+        private static async Task HandleServerToClient(IServerStreamWriter<ServerToClientMessage> responseStream, ServerCallContext context)
+        {
+            var pingCount = 0;
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new ServerToClientMessage
+                {
+                    Text = $"Service Said hi {pingCount} times",
+                    Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+                });
+            }
+        }
+
+        private async Task HandleClientToServerPing(IAsyncStreamReader<ClientToServerMessage> requestStream)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+                _logger.LogInformation($"The client said {message.Text}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ GrpcService.Server/Services/WeatherProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcService.Server.Services
+{
+    public class WeatherProvider : WeatherService.WeatherServiceBase
+    {
+        // This is the service name we defined in the proto file - GetWeather
+        public override Task<WeatherResponse> GetWeather(GetCityWeather request, ServerCallContext context)
+        {
+            var city = request.City;
+            var unit = request.Unit;
+            
+            // Do the service logic here...
+            
+            return Task.FromResult(new WeatherResponse
+            {
+                Temperature = 123,
+                FeelsLike = 567
+            });
+        }
+    }
+}
